2e3d3df313b6c78a2e13adb308a2c901142facf3
Laura McCormick <laura.mccormick@bazaarvoice.com>
2025-03-13 11:04:31 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import dogBanner from "./assets/dogbanner.gif";
+import simpleThings from "./assets/simplethings.gif";
+import pixelCoffee from "./assets/pixelcoffee.webp";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-content">
+        <header className="App-header">
+          <div>
+            <a href="/" style={{ textDecoration: "none" }}>
+              Nav 1
+            </a>{" "}
+            <img
+              src={pixelCoffee}
+              alt=""
+              style={{ marginLeft: "3px", marginRight: "5px" }}
+            />
+            <a href="/" style={{ textDecoration: "none" }}>
+              Nav 2
+            </a>{" "}
+            <img
+              src={pixelCoffee}
+              alt=""
+              style={{ marginLeft: "3px", marginRight: "5px" }}
+            />
+            <a href="/" style={{ textDecoration: "none" }}>
+              Nav 3
+            </a>
+          </div>
+          <img src={dogBanner} alt="Dog running" />
+        </header>
+
+        <div className="App-text">
+          <h1 style={{ color: "lightsalmon" }}>Laura McCormick</h1>
+          <img src={simpleThings} alt="It's the simple things" />
+          <h2 style={{ color: "darksalmon" }}>Welcome to my page!!</h2>
+        </div>
+      </div>
     </div>
   );
 }
